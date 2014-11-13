eec6ad2a56e627de69a95a87bbff4ac9492f21de
Nicolas Besnard <besnard.nicolas@gmail.com>
2014-11-13 21:26:18 +0100

    Add informations to Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141112153641) do
+ActiveRecord::Schema.define(version: 20141113202235) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -28,6 +28,12 @@ ActiveRecord::Schema.define(version: 20141112153641) do
     t.datetime "updated_at"
     t.string   "token",                  default: "", null: false
     t.date     "token_expiration"
+    t.string   "firstname"
+    t.string   "lastname"
+    t.integer  "gender"
+    t.date     "birtdate"
+    t.integer  "phone"
+    t.integer  "pincode"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
