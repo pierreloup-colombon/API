0d4f748a63e9ee0a9de4614afb0f20044864c64e
Nicolas Besnard <besnard.nicolas@gmail.com>
2015-02-18 19:48:53 +0000

    WristbandController: authentication

--- app/controllers/api/v1/wristbands_controller.rb
+++ app/controllers/api/v1/wristbands_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::WristbandsController < Api::V1::BaseController
-  before_action :authenticate_user!, only: [ :create ]
+  before_action :authenticate_user!, only: [ :create, :add_to_user ]
 
   def create
     render json: Wristband.create.as_json(root: true, only: [ :id ])
